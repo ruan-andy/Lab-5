ed1a9c3fd5f13dd93630563774997e227f996cf6
Andy Ruan <a.ruan2016@gmail.com>
2018-05-15 23:31:52 -0700

    Initial commit

---
+++ TestingWithJson/Song.java
@@ -0,0 +1,15 @@
+import java.io.Serializable;
+
+public class Song {
+  private name;
+  private status;
+  private id;
+  private loc;
+
+  public Song(int location) {
+    name = location.toString();
+    status = 0;
+    loc = location;
+    id =
+  }
+}
